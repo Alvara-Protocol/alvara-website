2245e0e77be5118b4835d3d7c89a811a2cc22eae
gyroflaw <83718263+gyroflaw@users.noreply.github.com>
2022-10-04 22:05:43 +0900

    fix: Prettier options

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,20 @@
+/**
+ * @type {import('@types/prettier').RequiredOptions}
+ */
 module.exports = {
+  trailingComma: 'all',
+  printWidth: 80,
   arrowParens: 'always',
   singleQuote: true,
-  jsxSingleQuote: true,
+  jsxSingleQuote: false,
   tabWidth: 2,
   semi: true,
+  overrides: [
+    {
+      files: ['*.css', '*.scss'],
+      options: {
+        tabWidth: 4,
+      },
+    },
+  ],
 };
---
+++ src/views/Home/Title.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Button } from '@/components';
+
+export default function Title() {
+  return (
+    <div>
+      <section>
+        <h1 className="font-normal">ALVARA PROTOCOL</h1>
+        <q>
+          Delivering the world&apos;s most popular financial product to crypto.
+        </q>
+        <Button>ALVA</Button>
+      </section>
+    </div>
+  );
+}
