be20e6c6fa24308678f11ed9ab98094f2c95a75a
gyroflaw <83718263+gyroflaw@users.noreply.github.com>
2022-10-15 01:29:53 +0900

    fix: smart-funds page typo

--- src/views/Smartfunds/Description.tsx
+++ src/views/Smartfunds/Description.tsx
@@ -11,34 +11,47 @@ export default function Description() {
       <div className="h-[4px] w-1/2 bg-gradient-to-r from-purple-650 to-transparent"></div>
       <ul className="intro grid grid-cols-2 gap-x-[10rem] gap-y-12">
         <li>
-          I&apos;m a paragraph. Click here to add your own text and edit me.
-          I&apos;m a great place for you to tell a story and let your users know
-          a little more about you.
+          <b>Build Your Own.</b>
+          <br /> Design and launch your own Crypto Smart Fund via Alvara&apos;s
+          creation portal. Earn rewards each time your fund&apos;s shares are
+          traded on the HiveX.
         </li>
         <li>
-          I&apos;m a paragraph. Click here to add your own text and edit me.
-          I&apos;m a great place for you to tell a story and let your users know
-          a little more about you.
+          <b>Yield Generation.</b>
+          <br />
+          Every smart fund contains 100% collateralized underlying assets. These
+          assets are utilized in yield generation strategies via our partners,
+          with the rewards being fed back into the fund and distributed to share
+          holders as dividends.
         </li>
         <li>
-          I&apos;m a paragraph. Click here to add your own text and edit me.
-          I&apos;m a great place for you to tell a story and let your users know
-          a little more about you.
+          <b>Proof of Ownership.</b>
+          <br />
+          NFT integration provides smart fund managers with a key to their
+          fund&apos;s management rights and rewards structure. Transferable at
+          Alvara&apos;s NFT marketplace, fund managers can, for the first time,
+          sell their business.
         </li>
         <li>
-          I&apos;m a paragraph. Click here to add your own text and edit me.
-          I&apos;m a great place for you to tell a story and let your users know
-          a little more about you.
+          <b>First Mover Advantage.</b>
+          <br />
+          Alvara has first mover advantage, building the very first one-stop
+          protocol with a hybridized exchange, creation and management portal.
+          Bringing ETFs to DeFi like never before.
         </li>
         <li>
-          I&apos;m a paragraph. Click here to add your own text and edit me.
-          I&apos;m a great place for you to tell a story and let your users know
-          a little more about you.
+          <b>Advanced Leaderboard.</b>
+          <br />
+          Compete against your fellow fund managers for top ranking positions on
+          Alvara&apos;s leaderboard. Follow the most profitable fund managers to
+          minimize your risk exposure to the crypto markets.
         </li>
         <li>
-          I&apos;m a paragraph. Click here to add your own text and edit me.
-          I&apos;m a great place for you to tell a story and let your users know
-          a little more about you.
+          <b>Low Cost.</b>
+          <br />
+          Underlying assets are purchased through APIs linked to leading CEXs,
+          making the costs of creating, redeeming and rebalancing much cheaper
+          than fully on-chain alternatives.
         </li>
       </ul>
     </section>
--- src/views/Smartfunds/Intro.tsx
+++ src/views/Smartfunds/Intro.tsx
@@ -6,14 +6,17 @@ export default function Intro() {
   return (
     <section className="flex flex-col gap-28 bg-gray-400 pb-20">
       <Hives className="flex flex-row items-center justify-around px-11 pt-[250px]">
-        <h1 className="text-center text-[60px] leading-normal">
+        <h1 className="text-center text-[60px] uppercase leading-normal">
           Smart <br />
           funds.
         </h1>
         <div className="flex w-1/2 max-w-[600px] flex-col gap-10">
           <p className="font-poppins text-[20px] font-light tracking-widest">
-            Welcome visitors to your site with a short, engaging introduction.
-            Double click to edit and add your own text. honeycomb
+            Alvara SmartFunds are the name for our crypto ETFs. Offering
+            investors the world&apos;s most popular investment vehicle, but on
+            the blockchain. ETFs are number one for a reason - they provide a
+            low risk, low cost, diversified and transparent investment
+            opportunity.
           </p>
           <div className="h-[4px] w-4/5 bg-gradient-to-r from-purple-650 to-transparent"></div>
           <Button variant="outline" className="max-w-max uppercase">
@@ -24,29 +27,29 @@ export default function Intro() {
       <div className="container mx-auto grid grid-cols-4 px-20">
         <Info
           image="/images/smart-funds/build.png"
-          title="Build your Own"
-          description="Describe the item and include any relevant details. Click to edit the text"
+          title="Low Risk"
+          description="Risk is spread through investing in multiple assets and asset classes."
           size={114}
           type={1}
         />
         <Info
           image="/images/smart-funds/proof.png"
-          title="Proof of Ownership"
-          description="Describe the item and include any relevant details. Click to edit the text"
+          title="Low Cost"
+          description=" Management and operating expenses lower than other investment vehicles."
           size={114}
           type={1}
         />
         <Info
           image="/images/smart-funds/leaderboard.png"
-          title="Advanced leaderboard"
-          description="Describe the item and include any relevant details. Click to edit the text"
+          title="Transparent"
+          description="An ETF’s holdings and weightings are disclosed publicly and regularly."
           size={114}
           type={1}
         />
         <Info
           image="/images/smart-funds/join.png"
-          title="Join the Hive"
-          description="Describe the item and include any relevant details. Click to edit the text"
+          title="Diversificaction"
+          description="ETFs offer instant diversification through exposure to a basket of underlying assets."
           size={114}
           type={1}
         />
