c1419c05550cb3b7febc6e5cb33d9cd69957db30
gyroflaw <83718263+gyroflaw@users.noreply.github.com>
2022-10-15 03:48:47 +0900

    fix: font issue

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,11 +17,13 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link
-            rel="preload"
-            href="/fonts/Montserrat/Montserrat-Medium.ttf"
-            as="font"
-            type="font/ttf"
-            crossOrigin="anonymous"
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Poppins:wght@300;400;500;700&display=swap"
           />
         </Head>
         <body>
