b5c40f59b4030a6d29bef5357cec03350ab1328c
gyroflaw <83718263+gyroflaw@users.noreply.github.com>
2022-10-15 00:20:07 +0900

    Revert "fix: build ssr issue"
    This reverts commit b0a44089e1d0757ee6fe52224d5b1540900d1aff.

--- src/hooks/usePresaleContract.ts
+++ src/hooks/usePresaleContract.ts
@@ -1,18 +1,15 @@
 import { AlvrTokenSale, AlvrTokenSale__factory } from '@alvara/presale';
-import { chain, useContract, useSigner } from 'wagmi';
-
-import { provider } from '@/lib/web3/wagmi';
+import { useContract, useSigner } from 'wagmi';
 
 import { CHAIN_ID, contractAddress } from '@/config';
 
 export default function usePresaleContract() {
   const { data: signer } = useSigner();
-  const isSSR = typeof window === undefined ? true : false;
 
   const contract: AlvrTokenSale = useContract({
     addressOrName: contractAddress[CHAIN_ID],
     contractInterface: AlvrTokenSale__factory.createInterface(),
-    signerOrProvider: isSSR ? provider({ chainId: chain.goerli.id }) : signer,
+    signerOrProvider: signer,
   });
 
   return contract;
