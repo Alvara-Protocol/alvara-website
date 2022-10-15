f84d4ac1a23a9bebc7d08fea04dd9f18677d8519
gyroflaw <83718263+gyroflaw@users.noreply.github.com>
2022-10-15 18:49:59 +0900

    fix: update teams components

--- src/views/Team/components/Member.tsx
+++ src/views/Team/components/Member.tsx
@@ -1,6 +1,7 @@
+import Image from 'next/image';
 import React from 'react';
 
-import { Arrow2Icon, LinkedinIcon, NextImage } from '@/components';
+import { Arrow2Icon, LinkedinIcon } from '@/components';
 import UnstyledLink from '@/components/links/UnstyledLink';
 
 export interface SocialLinks {
@@ -22,18 +23,20 @@ export default function Member({
   links,
 }: MemberProps) {
   return (
-    <div className="border-gradient2 flex max-w-max flex-col items-center justify-around gap-6 border-2 bg-gray-400 py-4 px-8">
-      <div className="flex flex-col items-center gap-4">
-        <NextImage
-          className="mb-4"
+    <div className="border-gradient2 gap-6 border-2 bg-gray-400 py-4 px-8 text-center">
+      <div className="">
+        <Image
+          className="mb-4 "
           src={photo}
+          layout="responsive"
           width={271}
           height={271}
           alt={name}
         />
-        <div className="font-proppine text-2xl font-medium">{name}</div>
-        <div className="text-purple-700">{designation}</div>
       </div>
+      <div className="font-proppine mt-3 text-2xl font-medium">{name}</div>
+      <div className="text-purple-700">{designation}</div>
+
       <div className="flex w-full flex-row items-center justify-between">
         <div className="flex flex-row items-center gap-2">
           {links?.linkedin && (
--- src/views/Team/index.tsx
+++ src/views/Team/index.tsx
@@ -56,17 +56,9 @@ export default function Team() {
             Alvara teams
           </h2>
           <div className="h-[2px] w-3/5 bg-gradient-to-r from-purple-650 to-transparent"></div>
-          <p>
-            Why is the exchange so good? looks nice, easy to use, web3 - link
-            metamask, secure, ability to trade ETFs, ability to build (only
-            place to build), facilitate onboarding with HiveMind trading. Add a
-            general description of the items listed below. You can introduce the
-            list and include any relevant information you want to share. Double
-            click to edit the text.
-          </p>
         </div>
       </Hives>
-      <section className="container mx-auto flex flex-wrap items-center justify-center gap-x-28 gap-y-20 p-20">
+      <section className="container mx-auto mt-32 mb-10 grid grid-cols-4 gap-x-10 gap-y-16 ">
         {members.map((member, i) => (
           <Member key={i} {...member} />
         ))}
