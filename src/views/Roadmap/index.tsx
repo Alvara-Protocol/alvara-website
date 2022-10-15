0982299881d7b0dc82849f4452c5b4624064338e
gyroflaw <83718263+gyroflaw@users.noreply.github.com>
2022-10-15 17:05:09 +0900

    fix: add link to whitepaper & deck

--- src/views/Roadmap/index.tsx
+++ src/views/Roadmap/index.tsx
@@ -10,28 +10,32 @@ export default function Roadmap() {
       <Hives className="bg-gray-400 px-20 py-28">
         <Timeline />
       </Hives>
-      <div className="container mx-auto flex flex-row items-center justify-between gap-28 p-8">
-        <div className="border-gradient2 flex w-1/2 flex-col items-center gap-8 border-2 bg-gray-400 px-8 py-28">
-          <h2>Whitepaper</h2>
-          <div className="h-[4px] w-4/5 bg-gradient-to-r from-purple-650 to-transparent"></div>
-          <p className="max-w-[350px] text-center font-poppins text-[20px] font-light tracking-widest">
+      <div className="container mx-auto grid grid-cols-1 gap-28 p-8 md:grid-cols-2">
+        <div className="border-gradient2  border-2 bg-gray-400 px-8 py-28 text-center">
+          <h2 className="text-center">Whitepaper</h2>
+          <div className="mx-auto mb-8 h-[4px] w-4/5 bg-gradient-to-r from-purple-650 to-transparent "></div>
+          <p className="mx-auto  mb-8 max-w-[350px] text-center font-poppins text-sm  font-light tracking-widest md:text-[20px]">
             Thorough description of the Alvara Protocol
           </p>
 
-          <Button variant="outline" className="max-w-max uppercase">
-            Read more
-          </Button>
+          <a href="/whitepaper.pdf" target="_blank" rel="noreferrer">
+            <Button variant="outline" className="max-w-max uppercase">
+              Read more
+            </Button>
+          </a>
         </div>
-        <div className="border-gradient2 flex w-1/2 flex-col items-center gap-8 border-2 bg-gray-400 px-8 py-28">
-          <h2>Deck.</h2>
-          <div className="h-[4px] w-4/5 bg-gradient-to-r from-purple-650 to-transparent"></div>
-          <p className="max-w-[350px] text-center font-poppins text-[20px] font-light tracking-widest">
+        <div className="border-gradient2  border-2 bg-gray-400 px-8 py-28 text-center">
+          <h2 className="text-center">Deck.</h2>
+          <div className="mx-auto  mb-8 h-[4px] w-4/5 bg-gradient-to-r from-purple-650 to-transparent"></div>
+          <p className="mx-auto  mb-8 max-w-[350px] text-center font-poppins text-sm font-light tracking-widest md:text-[20px]">
             A quick glance at what makes Alvara so sweet
           </p>
 
-          <Button variant="outline" className="max-w-max uppercase">
-            Read more
-          </Button>
+          <a href="/deck.pdf" target="_blank" rel="noreferrer">
+            <Button variant="outline" className="max-w-max uppercase ">
+              Read more
+            </Button>
+          </a>
         </div>
       </div>
     </main>
