152e38ad32cc2dc93c118bc1008ab65ce6d7c05a
gyroflaw <83718263+gyroflaw@users.noreply.github.com>
2022-10-15 18:52:02 +0900

    fix: update teams title

--- src/views/Team/index.tsx
+++ src/views/Team/index.tsx
@@ -51,11 +51,11 @@ export default function Team() {
   return (
     <main>
       <Hives className="flex w-full flex-col items-center">
-        <div className="flex max-w-[560px] flex-col items-center gap-12 pt-[150px]">
-          <h2 className=" w-1/2 text-center font-montserrat font-medium uppercase tracking-widest">
+        <div className=" pt-[150px] text-center">
+          <h2 className="text-center font-montserrat font-medium uppercase tracking-widest">
             Alvara teams
           </h2>
-          <div className="h-[2px] w-3/5 bg-gradient-to-r from-purple-650 to-transparent"></div>
+          <div className="mx-auto mt-5 h-[2px] w-3/5 bg-gradient-to-r from-purple-650 to-transparent"></div>
         </div>
       </Hives>
       <section className="container mx-auto mt-32 mb-10 grid grid-cols-4 gap-x-10 gap-y-16 ">
