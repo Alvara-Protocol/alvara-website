ccc4cf24bcc7690ae22024fb894182b2079189c7
gyroflaw <83718263+gyroflaw@users.noreply.github.com>
2023-07-14 00:46:41 -0500

    fix: fixed parter z index

--- src/views/Home/Partners.tsx
+++ src/views/Home/Partners.tsx
@@ -14,51 +14,37 @@ export default function Partners() {
           width="100"
           heigth="70"
           name="LATCH"
-          zIndex="-z-40"
         />
 
-        <Partner
-          image="/images/partners/chainlink.svg"
-          name="LATCH"
-          zIndex="-z-30"
-        />
+        <Partner image="/images/partners/chainlink.svg" name="LATCH" />
         <Partner
           image="/images/partners/dextools.svg"
           name="LATCH"
           width="200"
           heigth="200"
-          zIndex="-z-20"
         />
         <Partner
           image="/images/partners/ferrum-network.svg"
           width="200"
           heigth="200"
           name="LATCH"
-          zIndex="-z-10"
         />
-        <Partner
-          image="/images/partners/fireblocks.svg"
-          name="LATCH"
-          zIndex="z-0"
-        />
+        <Partner image="/images/partners/fireblocks.svg" name="LATCH" />
 
         <Partner
           image="/images/partners/parsiq.svg"
           heigth="190"
           name="LATCH"
-          zIndex="z-10"
         />
         <Partner
           image="/images/partners/clintex.png"
           name="LATCH"
           width="120"
           heigth="30"
-          zIndex="z-20"
         />
         <Partner
           image="/images/partners/qan.svg"
           name="LATCH"
-          zIndex="z-30"
           width="130"
           heigth="30"
         />
@@ -67,7 +53,6 @@ export default function Partners() {
           image="/images/partners/vEmpire.png"
           name="LATCH"
           heigth="60"
-          zIndex="z-40"
         />
       </div>
     </section>
